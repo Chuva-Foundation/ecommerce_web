f74e53d3c0b6c6d306d9850e2e7542e57e6715cc
Kenny Rocha Gonçalves <105680254+K-goncalves@users.noreply.github.com>
2022-11-24 00:47:02 -0100

    Updated Header and Footer
    - Changed some CSS on navbar and footer;
    - Added "all rights reserved" to footer
    - Tried to create a dynamic menu

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,10 @@
+import styles from "./Footer.module.css";
+
+const date = new Date();
+const year = date.getFullYear();
+
 function Footer() {
-  return <div>Footer</div>;
+  return <div className={styles.footer}>&copy; {year} All Rights Reserved</div>;
 }
 
 export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import { HiOutlineMenu } from "react-icons/hi";
@@ -5,38 +6,42 @@ import styles from "./Header.module.css";
 
 const logo = (
   <div>
-    <Link to="/" className={styles.link}>
+    <Link to="/" className={styles.logo}>
       <h2>
-        Jack <span className={styles.span}>Ecom</span>
+        Jack <span className={styles.ecom}>Ecom</span>
       </h2>
     </Link>
   </div>
 );
 
 const cart = (
-  <span>
+  <span className={styles.cart}>
     <Link to="/cart">
       cart
       <FaShoppingCart size={20} />
-      <p>3</p>
+      <p className={styles["cart-orders"]}>3</p>
     </Link>
   </span>
 );
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header>
       <div className={styles.header}>
         {logo}
+
         <nav>
+          <div></div>
           <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
             <Link to="/contact">Contact Us</Link>
           </ul>
-          <div className={styles.headerright}>
-            <span>
+          <div className={styles["header-right"]}>
+            <span className={styles.links}>
               <Link to="/login">Login</Link>
               <Link to="/register">Register</Link>
               <Link to="/order-history">My orders</Link>
@@ -45,7 +50,7 @@ function Header() {
           </div>
         </nav>
 
-        <div>
+        <div className={styles["menu-icon"]}>
           {cart}
           <HiOutlineMenu size={20} />
         </div>
