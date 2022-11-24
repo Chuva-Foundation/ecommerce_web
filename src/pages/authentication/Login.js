aa74d25026bf5e1af11d15aa09c2fc329774e904
Kenny Rocha Gonçalves <105680254+K-goncalves@users.noreply.github.com>
2022-11-24 14:27:33 -0100

    Updated login page
    - Added some minor CSS

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,7 +1,29 @@
 import styles from "./authentication.module.css";
+import login from "../../images/login.webp";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <section></section>;
+  return (
+    <section className={styles.authentication}>
+      <div>
+        <img src={login} alt="login" width={500} />
+      </div>
+      <div>
+        <h2>Login</h2>
+        <form>
+          <input type="text" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <p>
+            <button>Login</button>
+          </p>
+        </form>
+        <span>
+          <p>Don't have an account?</p>
+          <Link to="/register">Register</Link>
+        </span>
+      </div>
+    </section>
+  );
 }
 
 export default Login;
