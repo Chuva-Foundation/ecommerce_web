b3eb6065c22f86dcc57e44f3b2a1ae8bbc856fd9
Kenny Rocha Gonçalves <105680254+K-goncalves@users.noreply.github.com>
2022-11-27 18:49:17 -0100

    Updated axios and .env
    - Minor updates do axios regarding the base url of the API

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default axiosInstance;
