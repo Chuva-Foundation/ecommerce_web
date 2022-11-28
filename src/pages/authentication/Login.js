8272bc88aed25804ed3df4a07b90680a0c0e7c96
Kenny Rocha Gonçalves <105680254+K-goncalves@users.noreply.github.com>
2022-11-28 14:17:09 -0100

    Update Login.js
    - Input boxes is cleared if login was success;
    - Reorganized a couple imports;
    - Added toastify from react to display notification messages

--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,8 +1,9 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import axios from "../../service/axios";
 import styles from "./Authentication.module.css";
 import login from "../../images/login.webp";
-import { Link } from "react-router-dom";
-import axios from "../../service/axios";
-import { useState } from "react";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -20,46 +21,57 @@ function Login() {
         localStorage.setItem("userToken", response.data.token);
 
         const storageToken = localStorage.getItem("userToken"); //Token saved in local Storage
-        console.log(storageToken);
+        toast.success("Welcome Back!");
+
         return response.data;
       });
+      //Clear inputbox if register was successfully
+      setEmail("");
+      setPassword("");
     } catch (error) {
+      toast.error("Wrong Password!");
       console.log(error);
     }
   };
   return (
-    <section className={styles.authentication}>
-      <div>
-        <img src={login} alt="login" width={500} />
-      </div>
-      <div className={styles.form}>
-        <h2>Login</h2>
-        <form onSubmit={loginUser}>
-          <input
-            type="email"
-            placeholder="Email"
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <p>
-            <button type="submit">Login</button>
-          </p>
-        </form>
-        <span>
-          <p>
-            Don't have an account?<Link to="/register"> Register</Link>
-          </p>
-        </span>
-      </div>
-    </section>
+    <>
+      <ToastContainer autoClose={3000} />
+      <section className={styles.authentication}>
+        <div>
+          <img src={login} alt="login" width={500} />
+        </div>
+
+        <div className={styles.form}>
+          <h2>Login</h2>
+
+          <form onSubmit={loginUser}>
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <p>
+              <button type="submit">Login</button>
+            </p>
+          </form>
+
+          <span>
+            <p>
+              Don't have an account?<Link to="/register"> Register</Link>
+            </p>
+          </span>
+        </div>
+      </section>
+    </>
   );
 }
 
