d9a9b206263cd968b7420ac68847c110459862c8
Kenny Rocha Gonçalves <105680254+K-goncalves@users.noreply.github.com>
2022-11-29 00:36:06 -0100

    Update Loader.js
    - Import of ReactDOM was wrong (fixed!);
    - Added the styles to the loader component;

--- src/components/loader/Loader.js
+++ src/components/loader/Loader.js
@@ -1,11 +1,11 @@
-import { ReactDOM } from "react";
+import ReactDOM from "react-dom";
 import styles from "./Loader.module.css";
 import loading from "../../images/loading.gif";
 
 function Loader() {
   return ReactDOM.createPortal(
-    <div>
-      <div>
+    <div className={styles.wrap}>
+      <div className={styles.load}>
         <img src={loading} alt="Loading..." />
       </div>
     </div>,
